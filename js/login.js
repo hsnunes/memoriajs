4403678ae7cad842924f79ba60a8b954d5e9a23e
Hilder Nunes <hildersn@gmail.com>
2026-05-26 15:55:06 -0300

    Outro link para teste

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@ const handleSubmit = (e) => {
     e.preventDefault();
     // Lógica para lidar com o envio do formulário
     localStorage.setItem('gamer', inputNome.value);
-    window.location = "./memoriajs/pages/game.html";
+    window.location = "./pages/game.html";
 }
 
 inputNome.addEventListener('input', validateInput);
